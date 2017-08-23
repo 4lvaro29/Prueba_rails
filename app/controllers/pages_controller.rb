4062b3e60686e7755dd3a25cd69c836e011716d3
Alvaro Urbina <4lvaro.up@gmail.com>
2017-08-23 13:43:49 -0300

    last

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,31 @@
 class PagesController < ApplicationController
+ 
   def batman
+
+    if params[:name].present? && params[:email].present?
+
+      User.create(name: params[:name], email: params[:email])
+
+      else
+
+      redirect_to pages_batman_path, alert:"Los campos son obligatorios"  
   end
 
   def superman
+
+    if params[:name].present? && params[:email].present?
+
+      User.create(name: params[:name], email: params[:email])
+
+      else
+
+      redirect_to pages_superman_path, alert:"Los campos son obligatorios"  
+
   end
 
   def batman_vs_superman
   end
 
+  end
+end
 end
